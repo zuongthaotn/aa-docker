dd7245980eae4362c1b7225921dd0c887f04a2cd
Zuongthao <zuongthao.tn@gmail.com>
2026-03-23 07:03:56 +0700

    add autofcholv

--- start-docker.sh
+++ start-docker.sh
@@ -30,6 +30,8 @@ fi
 #
 cd ..
 #
+docker container prune -f
+#
 if [ -z "$(docker images -q $DOCKER_IMAGE 2> /dev/null)" ]; then
     echo "Image $DOCKER_IMAGE does not exist. Building without cache..."
     docker compose build --no-cache
